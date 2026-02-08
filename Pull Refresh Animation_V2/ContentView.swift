15e98f1c7d6f5cffbc7395fba8e08dfc8d56405e
Ivan Voznyi <ivan.voznyi92@gmail.com>
2026-02-08 23:10:19 +0200

    Initial Commit

---
+++ Pull Refresh Animation_V2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pull Refresh Animation_V2
+//
+//  Created by Ivan Voznyi on 2/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pull Refresh Animation_V2/Pull_Refresh_Animation_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Pull_Refresh_Animation_V2App.swift
+//  Pull Refresh Animation_V2
+//
+//  Created by Ivan Voznyi on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Pull_Refresh_Animation_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
